38591cd2b29d98ac84f98ba857a8de1aa40cda90
Nur Khairusy <nurkhairusy@gmail.com>
2018-04-28 23:01:14 +0700

    new routing

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,52 @@ Route::get('foo', function () {
     return 'Hello World';
 });
 
-Route::get('/user', 'UserController@index');
+// Route::get('/user', 'UserController@index');
+
+
+$uri = "/cek";
+$callback = "UserController@something";
+Route::get($uri, $callback);
+Route::post($uri, $callback);
+Route::put($uri, $callback);
+Route::patch($uri, $callback);
+Route::delete($uri, $callback);
+Route::options($uri, $callback);
+
+
+
+Route::match(['get', 'post'], '/', function () {
+    //
+});
+
+Route::any('foo', function () {
+    //
+});
+
+//redirect /here to url /there, 301 is the redirect error
+Route::redirect('/here', '/there', 301);
+
+
+Route::view('/welcome', 'welcome');
+
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+// REQUIRED PARAMETERS
+// Route::get('user/{id}', function ($id) {
+//     return 'User '.$id;
+// });
+
+
+Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'post id: '.$postId.' and comment id: '.$commentId;
+});
+
+// OPTIONAL PARAMETERS
+Route::get('user/{name?}', function ($name = null) {
+    return $name;
+});
+
+// Route::get('user/{name?}', function ($name = 'John') {
+//     return $name;
+// });
+
